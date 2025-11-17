02e61123de04e493e1d5ee7c0fa882fb2ee2e35d
continue <1545620489@qq.com>
2025-11-17 20:47:54 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+</script>
+
+<style scoped></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>欢迎来到我们的爱情纪念册</h1>
+
+    <section>
+      <h3>留言板</h3>
+      <p>在这里留下你对我们的祝福和回忆吧！</p>
+    </section>
+  </div>
+</template>
+<script setup></script>
+<style scoped>
+.home {
+  padding: 16px;
+}
+</style>
